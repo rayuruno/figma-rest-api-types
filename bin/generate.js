eb726e63712d565f6c6cb888788d08f4d8f3e124
Jakub Hajduk (PGS Software) <jhajduk@pgs-soft.com>
2023-08-31 12:00:59 +0200

    update field id's, add wait for js to load, add some user feedback for cli

--- bin/generate.js
+++ bin/generate.js
@@ -4,9 +4,13 @@ import fetch from "../cmd/fetch.js";
 import generate from "../cmd/generate.js";
 import { writeFile } from "fs/promises";
 
-const cnt = await generate(await fetch());
+console.log('Waiting for Figma REST API documentation page and assets...')
+const page = await fetch()
+console.log('Generating types...')
+const cnt = await generate(page);
 const dst = process.argv[2];
 if (dst) {
+  console.log(`Writing ${dst} file...`)
   await writeFile(dst, cnt, "utf8");
 } else {
   process.stdout.write(cnt);
